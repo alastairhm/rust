6d53848923fbfe39065effa690c79f03f20bdac2
Alastair Montgomery <alastair.montgomery@engineering.digital.dwp.gov.uk>
2022-01-04 11:21:17 +0000

    Fibonacci

---
+++ fib_dyn.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+const FIB_ONE: u64 = 1;
+
+fn fib_dyn(n: u64, map: &mut HashMap<u64, u64>) -> u64 {
+    match n {
+        0 | 1 => FIB_ONE,
+        n => {
+            if map.contains_key(&n) {
+                *map.get(&n).unwrap()
+            } else
+            {
+                let val = fib_dyn(n-1, map) + fib_dyn(n-2, map);
+                map.insert(n, val);
+                val
+            }
+        }
+    }
+} 
+
+fn main() {
+    let mut map = HashMap::new();
+    for i in 1..41 {
+        println!("{} {}",i, fib_dyn(i, &mut map))
+    }
+}
+
