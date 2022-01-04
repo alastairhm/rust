8a809ace8307fc0506f6b71cc90f03bb9afa5bd7
Alastair Montgomery <alastair.montgomery@engineering.digital.dwp.gov.uk>
2022-01-04 10:24:57 +0000

    Linked List

---
+++ linkedlist.rs
@@ -0,0 +1,14 @@
+use std::collections::LinkedList;
+
+fn main() {
+    let mut ll = LinkedList::new();
+    
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(4);
+    
+    for foo in ll {
+        println!("{}", foo);
+    }
+}
+
