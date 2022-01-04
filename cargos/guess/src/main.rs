ba0b303500e7ce3898d8d82d017c5fac43e0a176
Alastair Montgomery <alastair@pop-os.localdomain>
2022-01-04 13:23:16 +0000

    Guessing game again

---
+++ cargos/guess/src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+fn main(){
+    println!("Welcome to the guessing game!");
+    println!("Input guess: "); 
+
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess);
+
+    println!("You guessed: {}", guess);
+}
